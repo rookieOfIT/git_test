8fac9b33b66844da710a61f3c99acb3ec23f5ac7
Levi <1654395037@qq.com>
2021-12-12 17:21:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author levi
+ * @create 2021-12-12 17:07
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
